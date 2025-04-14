fbf5d9e75097b35385868b108f9ae1669f2935c7
ieow <cwlee0@gmail.com>
2025-04-14 17:57:21 +0800

    feat: google SignIn android works

--- ios/GoogleAcm.swift
+++ ios/GoogleAcm.swift
@@ -5,4 +5,14 @@ class GoogleAcm: NSObject {
   func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
     resolve(a*b)
   }
+
+  @objc(signInWithGoogle:withResolver:withRejecter:)
+  func signInWithGoogle(params: NSDictionary, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    reject("ERROR", "Not Supported", nil)
+  }
+
+  @objc(signOut:withResolver:withRejecter:)
+  func signOut( a: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    reject("ERROR", "Not Supported", nil)
+  }
 }
